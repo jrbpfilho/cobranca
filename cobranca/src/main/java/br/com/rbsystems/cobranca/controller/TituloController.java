94bc129d7351fa29d39bad30381664ed43f6473a
caelum <caelum@FJ21>
2017-04-10 17:08:40 -0300

    First Commit - Cobrança

---
+++ cobranca/src/main/java/br/com/rbsystems/cobranca/controller/TituloController.java
@@ -0,0 +1,94 @@
+package br.com.rbsystems.cobranca.controller;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import br.com.rbsystems.cobranca.model.StatusTitulo;
+import br.com.rbsystems.cobranca.model.Titulo;
+import br.com.rbsystems.cobranca.repository.Titulos;
+
+
+
+@Controller
+@RequestMapping("/titulos")
+public class TituloController {
+	
+	private static final String CADASTRO_VIEW = "CadastroTitulo";
+	
+	@Autowired
+	private Titulos titulos;
+	
+	
+	
+	@RequestMapping("/novo")
+	public ModelAndView novo() {
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
+		mv.addObject(new Titulo());
+		mv.addObject("todosStatusTitulo", StatusTitulo.values());
+		return mv;
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	/*
+	 * Código anterior para redirect
+	 * public ModelAndView salvar(@Validated Titulo titulo, Errors errors) {
+	 */
+	
+	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) {
+		
+		/* Código anterior para redirect
+		
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		
+		if(errors.hasErrors()) {
+			return mv;
+		}
+		
+		*/
+		
+		if(errors.hasErrors()) {
+			return "CadastroTitulo";
+		}
+		
+		System.out.println(">>> " + titulo.getDescricao());
+		titulos.save(titulo);
+		attributes.addFlashAttribute("mensagem", "Título salvo com sucesso!!!!");
+		return "redirect:/titulos/novo";
+	}
+	
+	@RequestMapping
+	public ModelAndView pesquisar() {
+		List<Titulo> todosTitulos = titulos.findAll();
+		ModelAndView mv = new ModelAndView("PesquisaTitulos");
+		mv.addObject("titulos", todosTitulos);
+		return mv;
+	}
+	
+	@RequestMapping("{id}")
+	public ModelAndView edicao(@PathVariable("id") Titulo titulo) {
+		
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
+		mv.addObject(titulo);
+		return mv;
+		
+	}
+	
+		
+	@ModelAttribute("todosStatusTitulo")
+	public List<StatusTitulo> todosStatusTitulo() {
+		return Arrays.asList(StatusTitulo.values());
+	}
+}
+ 
---
+++ cobranca/src/main/java/br/com/rbsystems/cobranca/model/Titulo.java
@@ -0,0 +1,122 @@
+package br.com.rbsystems.cobranca.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.NumberFormat;
+
+@Entity
+public class Titulo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY )
+	private Long id;
+	
+	@NotEmpty(message = "Descrição é obrigatória")
+	@Size(max = 60, message = "A descrição não pode conter mais que 60 caracteres")
+	private String descricao;
+	
+	@NotNull(message = "Data de Vencimento é obrigatória")
+	@DateTimeFormat(pattern = "dd/MM/yyyy")
+	@Temporal(TemporalType.DATE)
+	private Date dataVencimento;
+	
+	@NotNull(message = "Valor é obrigatório")
+	@DecimalMin(value = "0.01", message = "Valor tem que ser maior que R$ 0.01")
+	@DecimalMax(value = "9999999999.99", message = "Valor tem que ser maior que R$ 9.999.999.999,99")
+	@NumberFormat(pattern ="#,##0.00")
+	private BigDecimal valor;
+	
+	@Enumerated(EnumType.STRING)
+	private StatusTitulo status;
+	
+	
+	private String local;
+	
+	public String getLocal() {
+		return local;
+	}
+	
+	public void setLocal(String local) {
+		this.local = local;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Date getDataVencimento() {
+		return dataVencimento;
+	}
+	public void setDataVencimento(Date dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+	public BigDecimal getValor() {
+		return valor;
+	}
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+	public StatusTitulo getStatus() {
+		return status;
+	}
+	public void setStatus(StatusTitulo status) {
+		this.status = status;
+	}
+	
+	public boolean isPendente(){
+		
+		return StatusTitulo.PENDENTE.equals(this.status);
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Titulo other = (Titulo) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+}
